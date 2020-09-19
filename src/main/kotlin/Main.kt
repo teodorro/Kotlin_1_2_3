83eb566a0add4296c68b7707a3bf7e2585d15c99
teodorro <edem.teo@gmail.com>
2020-09-19 16:59:14 +0300

    money endings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
-const val standardDiscount = 100;
+const val standardDiscount = 100_00;
 const val largeSumDiscount = 0.05;
 const val regularCustomerDiscount = 0.01;
-const val threshold1 = 1000;
-const val threshold2 = 10000;
+const val threshold1 = 1000_00;
+const val threshold2 = 10000_00;
 
 fun main() {
-    printDiscounts(50, 1000, true);
-    printDiscounts(1050, 1000, true);
-    printDiscounts(100500, 1000, true);
-    printDiscounts(50, 1000, false);
-    printDiscounts(1050, 1000, false);
-    printDiscounts(100500, 1000, false);
+    printDiscounts(50_00, 1001_33, true);
+    printDiscounts(1050_00, 1002_33, true);
+    printDiscounts(100500_00, 1003_33, true);
+    printDiscounts(50_00, 1004_36, false);
+    printDiscounts(1050_00, 1005_36, false);
+    printDiscounts(100500_00, 1006_36, false);
 }
 
 fun printDiscounts(previousBuyPrice: Int, nextStandardPrice: Int, isRegularCustomer: Boolean) {
@@ -30,14 +30,52 @@ fun printDiscounts(previousBuyPrice: Int, nextStandardPrice: Int, isRegularCusto
         (nextPriceAfterPreviousBuyDiscount * (1 - regularCustomerDiscount)).toInt() else
         nextPriceAfterPreviousBuyDiscount;
     val regularCustomerDiscountStr = if (isRegularCustomer) "1%" else "";
+
+    val previousBuyPriceStr = getMoneyString(previousBuyPrice);
+    val nextStandardPriceStr = getMoneyString(nextStandardPrice);
+    val nextPriceAfterPreviousBuyDiscountStr = getMoneyString(nextPriceAfterPreviousBuyDiscount);
+    val nextPriceAfterRegularCustomerDiscountStr = getMoneyString(nextPriceAfterRegularCustomerDiscount);
     println(
         (if (isRegularCustomer) "Постоянный покупатель\n" else "Случайный покупатель\n")
-                + "Предыдущие покупки: $previousBuyPrice руб\n"
-                + "Следующая покупка: $nextStandardPrice руб\n"
-                + (if (previousBuyDiscount > 0) "После $previousBuyDiscountStr скидки: $nextPriceAfterPreviousBuyDiscount руб\n" else "")
-                + (if (isRegularCustomer) "После $regularCustomerDiscountStr скидки: $nextPriceAfterRegularCustomerDiscount руб\n" else "")
+                + "Предыдущие покупки: $previousBuyPriceStr\n"
+                + "Следующая покупка: $nextStandardPriceStr\n"
+                + (if (previousBuyDiscount > 0) "После $previousBuyDiscountStr скидки: $nextPriceAfterPreviousBuyDiscountStr\n" else "")
+                + (if (isRegularCustomer) "После $regularCustomerDiscountStr скидки: $nextPriceAfterRegularCustomerDiscountStr\n" else "")
     );
+}
 
+fun getMoneyString(money: Int): String{
+    val kop = money % 100
+    val rub = money / 100;
+    val kopStr = getKopStr(kop)
+    if (rub == 0)
+        return kopStr;
+    val rubStr = getRubStr(rub)
+    return "$rubStr $kopStr";
+}
+
+fun getRubStr(rub: Int): String {
+    if (rub == 0)
+        return "";
+    val last = rub % 10;
+    val ending = when {
+        last == 1 && rub != 1 -> "рубль";
+        last in 2..4 && (rub < 10 || rub > 19) -> "рубля";
+        else -> "рублей"
+    }
+    return "$rub $ending";
+}
+
+fun getKopStr(kop: Int): String {
+    if (kop == 0)
+        return "";
+    val last = kop % 10;
+    val ending = when {
+        last == 1 && kop != 1 -> "копейка";
+        last in 2..4 && (kop < 10 || kop > 19) -> "копейки";
+        else -> "копеек"
+    }
+    return "$kop $ending";
 }
 
 
